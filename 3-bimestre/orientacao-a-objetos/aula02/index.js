ade9953594ef050e79892f8c2b2e138f68f18f82
Francisco Bruno <francisco.nascimento2192@aluno.ce.gov.br>
2026-08-10 23:56:16 +0000

    feito hj

---
+++ 3-bimestre/orientacao-a-objetos/aula02/index.js
@@ -0,0 +1,11 @@
+class Funcionarios{
+     constructor(nome, cargo, salario){
+        this.nome = nome
+        this.cargo = cargo
+        this.salario = salario
+     }
+}
+const funcionarioBruno = new Funcionarios("pedro", "pedreiro", 1000)
+
+
+module.exports = funcionarioBruno
